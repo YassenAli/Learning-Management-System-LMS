04c6ef037bd08281e9231106f66f454d1c07a777
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-12-13 10:21:46 +0200

    initialize version of course management

--- LearningManagementSystem/src/main/java/com/lms/LearningManagementSystem/repository/LessonRepository.java
+++ LearningManagementSystem/src/main/java/com/lms/LearningManagementSystem/repository/LessonRepository.java
@@ -1,7 +1,8 @@
 package com.lms.LearningManagementSystem.repository;
+
 import com.lms.LearningManagementSystem.model.Course;
 import com.lms.LearningManagementSystem.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
 }
