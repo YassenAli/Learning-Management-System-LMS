948748242edd51ed38fceb0f128490be1a5e237a
zm237338 <zm237338@gmail.com>
2024-12-16 07:41:50 +0200

    notifications are created in the mentioned cases

--- LearningManagementSystem/src/main/java/com/lms/LearningManagementSystem/dto/NotificationRequest.java
+++ LearningManagementSystem/src/main/java/com/lms/LearningManagementSystem/dto/NotificationRequest.java
@@ -1,15 +1,12 @@
 package com.lms.LearningManagementSystem.dto;
 
-import com.lms.LearningManagementSystem.model.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class NotificationRequest {
-    // Getters and setters
-    private User user;
+    private Long userId;
     private String subject;
     private String message;
-
 }
